c47b72288c71dd20d65599ae126c4389673d92f8
jslay88 <justin.slay@gmail.com>
2026-01-05 09:38:19 -0700

    Use different whitespace unicode char U+2800

--- am_bot/cogs/server_stats.py
+++ am_bot/cogs/server_stats.py
@@ -38,14 +38,14 @@ class ServerStatsCog(commands.Cog):
         self.guild = self.guild or self.bot.get_guild(GUILD_ID)
         channel = await self.bot.fetch_channel(MEMBERS_COUNT_CHANNEL_ID)
         member_count = len(self.guild.members)
-        await channel.edit(name=f"🔹┇{member_count}︲members")
+        await channel.edit(name=f"🔹┇{member_count}⠀members")
         logger.info(f"Updated member count: {member_count}")
 
     async def update_boost_count(self):
         self.guild = self.guild or self.bot.get_guild(GUILD_ID)
         channel = await self.bot.fetch_channel(BOOSTS_COUNT_CHANNEL_ID)
         boost_count = self.guild.premium_subscription_count
-        await channel.edit(name=f"🔸┇{boost_count}︲boosts")
+        await channel.edit(name=f"🔸┇{boost_count}⠀boosts")
         logger.debug(f"Updated boost count: {boost_count}")
 
     async def update_role_counts(self):
@@ -54,12 +54,12 @@ class ServerStatsCog(commands.Cog):
         modder_channel = await self.bot.fetch_channel(MODDER_STATS_CHANNEL_ID)
         modder_role = self.guild.get_role(MODDER_ROLE_ID)
         modder_count = len(modder_role.members)
-        await modder_channel.edit(name=f"🔸┇{modder_count}︲modders")
+        await modder_channel.edit(name=f"🔸┇{modder_count}⠀modders")
 
         mapper_channel = await self.bot.fetch_channel(MAPPER_STATS_CHANNEL_ID)
         mapper_role = self.guild.get_role(MAPPER_ROLE_ID)
         mapper_count = len(mapper_role.members)
-        await mapper_channel.edit(name=f"🔹┇{mapper_count}︲mappers")
+        await mapper_channel.edit(name=f"🔹┇{mapper_count}⠀mappers")
 
         logger.info(
             f"Updated role counts: {modder_count} modders, "
