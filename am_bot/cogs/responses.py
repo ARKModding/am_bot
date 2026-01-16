a1c60037864804f76f87b2c95de6a50593241263
O-S Marin <K07H@users.noreply.github.com>
2026-01-16 17:03:55 +0100

    Can now output multiple commands at once.
    Command handling now supports multiple commands at once if the `commands` attribute is defined in JSON.

--- am_bot/cogs/responses.py
+++ am_bot/cogs/responses.py
@@ -30,17 +30,36 @@ class ResponsesCog(commands.Cog):
             message.content[0] in COMMANDS
             and message.content[1:] in COMMANDS[message.content[0]]
         ):
+            # Grab command info from JSON.
             command = COMMANDS[message.content[0]][message.content[1:]]
-            if "duplicate" in command:
-                # Handle duplicate commands,
-                # grab original defined by `duplicate`
-                command = COMMANDS[message.content[0]][command["duplicate"]]
 
+            # Prepare the list of commands to send.
+            to_send = []
+            if "commands" in command:
+                # Handle multiple commands,
+                # grab the list of commands defined by `commands`.
+                commands_list = command["commands"].split(", ")
+                for current_command in commands_list:
+                    if current_command in COMMANDS[message.content[0]]:
+                        to_send.append(
+                            COMMANDS[message.content[0]][current_command]
+                        )
+            else:
+                to_send.append(command)
+            
             logger.info(f"Executing response command: {message.content}")
 
-            if "embed" in command:
-                await message.channel.send(
-                    embed=discord.Embed.from_dict(command["embed"])
-                )
-            elif "content" in command:
-                await message.channel.send(content=command["content"])
+            # Send messages.
+            for command_to_send in to_send:
+                final_cmd = command_to_send
+                if "duplicate" in final_cmd:
+                    # Handle duplicate commands,
+                    # grab original defined by `duplicate`.
+                    final_cmd = COMMANDS[message.content[0]][
+                        final_cmd["duplicate"]]
+                if "embed" in final_cmd:
+                    await message.channel.send(
+                        embed=discord.Embed.from_dict(final_cmd["embed"])
+                    )
+                elif "content" in final_cmd:
+                    await message.channel.send(content=final_cmd["content"])
